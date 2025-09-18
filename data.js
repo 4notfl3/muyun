50c9388d28eca1070c2b54faf8672fa12291b751
4notfl3 <2490782554@qq.com>
2025-09-18 19:27:51 +0800

    Add files via upload
    适配本地图片

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const organizationsData = [
     host: '任何帮会成员',
     time: '任何时间',
     location: '任何地点',
-    image: 'https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104430985_qdqqd_luzig2.avif'
+    image: 'img/日常/shier.jpg'
   },
   {
     id: 'ten_person_dungeon',
@@ -16,7 +16,7 @@ const organizationsData = [
     host: '美丽',
     time: '帅气',
     location: '小椋老师',
-    image: 'https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104434916_qdqqd_ow0atw.avif'
+    image: 'img/日常/fengliang.jpg'
   }
 ];
 
@@ -25,22 +25,22 @@ const demoVideos = [
   {
     id: '1',
     name: '帮会合照',
-    video:'https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104392754_qdqqd_nm6je1.avif'
+    video:'img/合照/hezhao2.jpg'
   },
   {
     id: '2',
     name: '帮会合照',
-    video: 'https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104384226_qdqqd_zthmfh.avif'
+    video: 'img/合照/hezhao4.jpg'
   },
   {
     id: '3',
     name: '帮会合照',
-    video: 'https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104388263_qdqqd_p0j49k.avif'
+    video: 'img/合照/hezhao1.jpg'
   },
   {
     id: '4',
     name: '帮会合照',
-    video: 'https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104377999_qdqqd_w2jqtq.avif'
+    video: 'img/合照/hezhao3.jpg'
   }
 ];
 
@@ -53,9 +53,9 @@ const characterData = [
     title: "沐云-沐澐-养老院创始人",
     desc: "打帮战，随我冲，面板还要提提\n特么的天赏石又保底，再也不充了",
     tags: ["权威", "站街", "招人"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101390668_qdqqd_5m7gzo.avif",
+    avatar: "avatars/44.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101877483_qdqqd_k5bw3a.avif"
+      "img/第一页/dangjia1.jpg",
     ]
   },
   {
@@ -64,9 +64,9 @@ const characterData = [
     title: "沐云第一防守指挥官",
     desc: "哈基九，进攻团还是不要你吗",
     tags: ["萌妹", "叫妈妈"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101308800_qdqqd_53le9k.avif",
+    avatar: "avatars/50.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103040194_qdqqd_6l42np.avif",
+      "img/第一页/yusao.jpg",
     ]
   },
   {
@@ -75,9 +75,9 @@ const characterData = [
     title: "第一神相",
     desc: "想要找金主的可以私聊我了每天给你五个交子",
     tags: ["无敌神相"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101319557_qdqqd_u07euk.avif",
+    avatar: "avatars/26.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103104066_qdqqd_f7dq08.avif",
+      "img/第一页/ganq.jpg",
     ]
   },
   {
@@ -86,8 +86,8 @@ const characterData = [
     title: "摆烂大王",
     desc: "世界以痛吻我 我伸舌头",
     tags: ["甜妹", "反差"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101347858_qdqqd_bhtj8r.avif",
-    art: ["https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103190900_qdqqd_p6r3kj.avif"]
+    avatar: "avatars/33.jpg",
+    art: ["img/第一页/yuezhang.jpg"]
   },
   {
     id: 'xian_huan_xing',
@@ -95,9 +95,9 @@ const characterData = [
     title: "无敌碎梦",
     desc: "极品碎梦大王",
     tags: ["无敌碎梦"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101313694_qdqqd_efep5u.avif",
+    avatar: "avatars/24.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103408054_qdqqd_9haks2.avif",
+      "img/第一页/xiaozi.jpg",
     ]
   },
   {
@@ -106,9 +106,9 @@ const characterData = [
     title: "企图砍死铁衣",
     desc: "碎梦加强！",
     tags: ["热心肠"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101395087_qdqqd_b6suvb.avif",
+    avatar: "avatars/46.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103848126_qdqqd_csp64t.avif",
+      "img/第一页/zuoxing.jpg",
   
     ]
   },
@@ -118,9 +118,9 @@ const characterData = [
     title: "虫吟大王",
     desc: "压力压力压力压力",
     tags: ["抽象"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101220599_qdqqd_t6vqzy.avif",
+    avatar: "avatars/11.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103880423_qdqqd_4yz1um.avif",
+      "img/第一页/ling.jpg",
     ]
   },
   {
@@ -129,9 +129,9 @@ const characterData = [
     title: "叶安",
     desc: "白宝有你的新年每一年",
     tags: ["社恐"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101343667_qdqqd_jg0mf0.avif",
+    avatar: "avatars/31.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103929402_qdqqd_jbdv2k.avif",
+      "img/第一页/yujung.jpg",
     ]
   },
   {
@@ -140,9 +140,9 @@ const characterData = [
     title: "最强统战",
     desc: " 灌伤害，骗骗花，拉开距离，回回啦\n给压压，再开大，压力有点大刚雷",
     tags: ["反差", "抽象", "神经病"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101388213_qdqqd_39rk1p.avif",
+    avatar: "avatars/43.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758102902326_qdqqd_nusyl5.avif",
+      "img/第一页/lanxing.jpg",
     ]
   },
   {
@@ -151,9 +151,9 @@ const characterData = [
     title: "我是刷子",
     desc: "球球不要给我大保底",
     tags: ["12偷吃"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101367110_qdqqd_yjyqur.avif",
+    avatar: "avatars/40.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103057116_qdqqd_p9soz0.avif",
+      "img/第一页/lingwang.jpg",
     ]
   },
   {
@@ -162,9 +162,9 @@ const characterData = [
     title: "不垚壁脸",
     desc: "我说白了老弟这你不进沐云/澐？",
     tags: ["招人这一块"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101337563_qdqqd_ikmuix.avif",
+    avatar: "avatars/28.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103125628_qdqqd_mxnmwz.avif",
+      "img/第一页/hua.jpg",
     ]
   },
   {
@@ -173,8 +173,8 @@ const characterData = [
     title: "血河人气次席",
     desc: "你去毛伤害是想等我对你怒吼吗",
     tags: ["种地菜农"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101393199_qdqqd_0aq6x4.avif",
-    art: ["https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103229425_qdqqd_q59tau.avif"]
+    avatar: "avatars/45.jpg",
+    art: ["img/第一页/huangyu.jpg"]
   },
   {
     id: 'yu_ban_sheng',
@@ -182,8 +182,8 @@ const characterData = [
     title: "药王谷权威御姐",
     desc: "以蛊为药，魂灯为引，  九灵有情人终成药人",
     tags: ["温柔", "认真"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101399972_qdqqd_3fihn0.avif",
-    art: ["https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103428291_qdqqd_xqj2q3.avif"]
+    avatar: "avatars/48.jpg",
+    art: ["img/第一页/chuyu.jpg"]
   },
   {
     id: 'zhen_jin_mian',
@@ -191,9 +191,9 @@ const characterData = [
     title: "吉祥物",
     desc: "李少大帅哥",
     tags: ["李少牛逼"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101350416_qdqqd_mi0blg.avif",
+    avatar: "avatars/34.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103860451_qdqqd_k88ay7.avif",
+      "img/第一页/lishao.jpg",
      
     ]
   },
@@ -203,9 +203,9 @@ const characterData = [
     title: "叶山",
     desc: "烛白宝有你每一年新年",
     tags: ["抽象", "反差", "表面社恐"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101367110_qdqqd_yjyqur.avif",
+    avatar: "avatars/40.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103910206_qdqqd_kwvi2n.avif",
+      "img/第一页/jiaoruo.jpg",
     ]
   },
   {
@@ -214,9 +214,9 @@ const characterData = [
     title: "沐云堂众",
     desc: "联赛重伤断层我就断触卡键 手抖 误触 帧率低 高延迟 瓶颈期 没手感 没心态 没状态 没支架 腱鞘炎犯了",
     tags: ["抽象", "应声虫", "站街"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101399972_qdqqd_3fihn0.avif",
+    avatar: "avatars/48.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103937351_qdqqd_ks232g.avif",
+      "img/第一页/yun.jpg",
     ]
   },
   {
@@ -225,12 +225,12 @@ const characterData = [
     title: "顶级辅助",
     desc: "如雾气弥漫，留下模糊的足迹，又悄然散去。",
     tags: ["帅气","美丽","小椋老师"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101357410_qdqqd_0a73rp.avif",
+    avatar: "avatars/37.jpg",
     art: [
       
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758102998261_qdqqd_hqis54.avif",
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758102994596_qdqqd_661a0q.avif",
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103000819_qdqqd_65x0oi.avif",
+      "img/第一页/fengliang2.jpg",
+      "img/第一页/fengliang1.jpg",
+      "img/第一页/fengliang4.jpg",
     ]
   },
   {
@@ -239,9 +239,9 @@ const characterData = [
     title: "沐云最强数据官",
     desc: "沐云棒棒的！",
     tags: ["品味好", "萌萌哒"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101397596_qdqqd_6bmstb.avif",
+    avatar: "avatars/47.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103080453_qdqqd_fr74fh.avif",
+      "img/第一页/ruanmei.jpg",
     ]
   },
   {
@@ -250,9 +250,9 @@ const characterData = [
     title: "蘸豆",
     desc: "蘸豆蘸豆蘸豆蘸豆蘸豆蘸豆蘸豆蘸豆蘸豆蘸豆蘸豆蘸豆蘸豆打死所有人",
     tags: ["蘸豆","小皇帝"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101341872_qdqqd_di6m1r.avif",
+    avatar: "avatars/30.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103163911_qdqqd_3kine1.avif",
+      "img/第一页/qizi.jpg",
 
     ]
   },
@@ -262,9 +262,9 @@ const characterData = [
     title: "虹桥百星王者",
     desc: "来玩自定义呀",
     tags: ["睡觉", "可爱", "衣品好"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101345637_qdqqd_3xqfys.avif",
+    avatar: "avatars/32.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103271899_qdqqd_ocaf8b.avif",
+      "img/第一页/taoyan.jpg",
     ]
   },
   {
@@ -273,9 +273,9 @@ const characterData = [
     title: "金戈铁马定仙原",
     desc: "白发疏疏映晓光，衣香鬓影立西厢。",
     tags: ["白发帅哥", "氛围感", "光影", "精致穿搭"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101352240_qdqqd_6o99wq.avif",
+    avatar: "avatars/35.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103443648_qdqqd_ssqwpv.avif",
+      "img/第一页/xianxue.jpg",
      
     ]
   },
@@ -285,8 +285,8 @@ const characterData = [
     title: "一只碎梦",
     desc: "爱江江",
     tags: ["高玩"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101359379_qdqqd_162ozt.avif",
-    art: ["https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103866325_qdqqd_ma6zg4.avif"]
+    avatar: "avatars/38.jpg",
+    art: ["第一页/qingque.jpg"]
   },
   {
     id: 'feng_yi',
@@ -294,9 +294,9 @@ const characterData = [
     title: "全能人机",
     desc: "为人仁厚，重情重义。",
     tags: ["御姐", "大王"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101298872_qdqqd_mw9rzo.avif",
+    avatar: "avatars/18.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103918570_qdqqd_2xk8v6.avif",
+      "img/第一页/shenyuan.jpg",
     ]
   },
   {
@@ -305,9 +305,9 @@ const characterData = [
     title: "国服梦泪",
     desc: " ⁶⁶⁶⁶⁶⁶ ⁶⁶ ₆₆₆₆真的太帅了吧₆₆₆₆⁶⁶⁶⁶⁶⁶⁶ ⁶⁶ ₆₆₆₆帅晕我了 ₆₆₆₆⁶6666 ⁶⁶⁶⁶⁶⁶ ⁶⁶ ₆₆₆₆666真的太帅了吧₆₆₆₆⁶太帅了⁶⁶⁶⁶⁶⁶ ⁶⁶ ₆₆₆₆₆₆₆₆⁶",
     tags: ["装货", "神", "压力"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101402468_qdqqd_pv1d0z.avif",
+    avatar: "avatars/49.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758103961415_qdqqd_iw7ga2.avif",
+      "img/第一页/baichuan.jpg",
     ]
   },
   //第二页！！！！
@@ -317,9 +317,9 @@ const characterData = [
     title: "超绝男神",
     desc: "沐云招人！",
     tags: ["糕手", "高冷"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101335430_qdqqd_i18d42.avif",
+    avatar: "avatars/27.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104141711_qdqqd_7ly6ah.avif",
+      "img/第二页/xi.jpg",
     ]
   },
   {
@@ -328,9 +328,9 @@ const characterData = [
     title: "副本开荒指挥",
     desc: "当0当1不如当3来的刺激",
     tags: ["cpdd", "打本中"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101304012_qdqqd_6prdet.avif",
+    avatar: "avatars/20.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104165691_qdqqd_6g4s8f.avif",
+      "img/第二页/xinlei.jpg",
     ]
   },
   {
@@ -423,9 +423,9 @@ const characterData = [
     title: "大帅比",
     desc: "尔若不离，我则不弃。",
     tags: ["帅气", "甜菜"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101350416_qdqqd_mi0blg.avif",
+    avatar: "avatars/34.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104154195_qdqqd_hryniq.avif",
+      "img/第二页/jiaduobao.jpg",
     ]
   },
   {
@@ -434,9 +434,9 @@ const characterData = [
     title: "极品奶龙",
     desc: "我以为尿得够高 就能够遮住眼泪",
     tags: ["猎奇", "少御"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101308800_qdqqd_53le9k.avif",
+    avatar: "avatars/22.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104172153_qdqqd_psardu.avif",
+      "img/第二页/naitu.jpg",
     ]
   },
   {
@@ -528,9 +528,9 @@ const characterData = [
     title: "高冷男神",
     desc: "阿玛特拉斯～～～",
     tags: ["高冷", "炫酷"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101152432_qdqqd_5bu72d.avif",
+    avatar: "avatars/3.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104158458_qdqqd_snrbur.avif",
+      "img/第二页/guhong.jpg",
     ]
   },
   {
@@ -539,9 +539,9 @@ const characterData = [
     title: "追进度的小仙女",
     desc: "你权衡利弊 考虑所有 唯独不考虑我",
     tags: ["好声音","沐云招人"],
-    avatar: "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758101175195_qdqqd_obnezn.avif",
+    avatar: "avatars/6.jpg",
     art: [
-      "https://s3plus.meituan.net/opapisdk/op_ticket_885190757_1758104177436_qdqqd_mnwnoi.avif",
+      "img/第二页/lu.jpg",
     ]
   },
   {
