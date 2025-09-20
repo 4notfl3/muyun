7b78bd8a3c7b1e80c6ba119bb7506ca0b5afa46f
4notfl3 <2490782554@qq.com>
2025-09-20 12:43:20 +0800

    Update data.js

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ const organizationsData = [
   {
     id: 'ten_person_dungeon',
     name: '收徒滴滴！',
-    description: '你养我小鹅不养你老',
+    description: '你养我小/不养你老',
     host: '',
     time: '',
     location: '',
